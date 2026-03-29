5578796747807fc5c16ea249b403a74c8ab43a3a
yan <yan428@mail.ru>
2026-03-29 22:46:56 +0300

    refacrot: http request optimization

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
+from collections import Counter
 import requests
 
 def get_text(url):
     response = requests.get(url)
     return response.text
 
-def count_word_frequencies(url, word):
-    text = get_text(url)
-    words = text.split()
-    count = 0
-    for w in words:
-        if w == word:
-            count += 1
-    return count
+def count_word_frequencies(text, words_to_count):
+    counter = Counter(text.split())
+    return {word: counter.get(word, 0) for word in words_to_count}
 
 def main():
     words_file = "words.txt"
     url = "https://eng.mipt.ru/why-mipt/"
 
-    words_to_count = []
     with open(words_file, 'r') as file:
-        for line in file:
-            word = line.strip()
-            if word:
-                words_to_count.append(word)
+        words_to_count = [line.strip() for line in file if line.strip()]
 
-    frequencies = {}
-    for word in words_to_count:
-        frequencies[word] = count_word_frequencies(url, word)
-    
+    text = get_text(url)
+    frequencies = count_word_frequencies(text, words_to_count)
+
     print(frequencies)
 
 if __name__ == "__main__":
